d980311afb6ef69f7a643eefb9e0c820df08c04f
Brad West <brad.g.west@gmail.com>
2023-11-15 15:59:52 -0700

    color is nice

--- src/notifier/local.py
+++ src/notifier/local.py
@@ -9,6 +9,23 @@ class LocalNotifierConfig(NamedTuple):
     pass
 
 
+class Colors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
+
 class LocalNotifier(Notifier):
     def _notify(self, org: Org, job: Job) -> None:
-        print(f"New job: {job.title} at {org.name}")
+        print(
+            "New job:"
+            f"{Colors.BOLD}{Colors.OKBLUE}{job.title}{Colors.ENDC}{Colors.ENDC}"
+            " at "
+            f"{Colors.OKCYAN}{org.name}{Colors.ENDC}"
+        )
