883f7439e094b4b5feba4d1c0f24cda43489a8f4
Brad West <brad.g.west@gmail.com>
2023-11-16 09:31:40 -0700

    Add job test

---
+++ tests/test_job.py
@@ -0,0 +1,9 @@
+from src.job import Job
+
+
+def test_job():
+    s = '{"title": "Aircraft Engineer", "url": "https://company_1.com/jobs/1"}'
+    j = Job("Aircraft Engineer", "https://company_1.com/jobs/1")
+
+    assert Job.from_json(s) == j
+    assert j.to_json() == s
